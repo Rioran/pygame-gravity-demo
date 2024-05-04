84e9fc269d08043975b14c445bea5a2690612338
slothGeorge <gosh20002@gmail.com>
2024-05-04 15:19:18 +0400

    feat: adding right and left wall

--- game/player.py
+++ game/player.py
@@ -3,9 +3,10 @@ from pathlib import Path
 import pygame
 
 
-#print(f'{__file__ = }, {Path().absolute() = }')
 CURRENT_FOLDER = Path(__file__).parent
 
+# print(f'{__file__ = }, {Path().absolute() = }')
+# print(f'{Path(__file__).parent = }')
 
 class Player(pygame.sprite.Sprite):
     def __init__(self):
@@ -15,24 +16,39 @@ class Player(pygame.sprite.Sprite):
         self.image = pygame.image.load(CURRENT_FOLDER / 'logo.png')  # .convert()
         self.rect = self.image.get_rect()
         self.y_speed = 0
+        self.x_speed = 0
 
         self.scene = pygame.display.get_surface().get_rect()
         print(f'area bottom = {self.scene.bottom}')
-        # self.rect.x = self.scene.centerx
         self.rect.centerx = self.scene.centerx
         self.rect.y = 100
 
+        self.scene = pygame.display.get_surface().get_rect()
+        print(f'area right = {self.scene.right}')
+        self.rect.centerx = self.scene.centerx
+        self.rect.x = 100
+
+        self.scene = pygame.display.get_surface().get_rect()
+        print(f'area left = {self.scene.left}')
+        self.rect.centerx = self.scene.centerx
+
+
     def update(self):
         self.y_speed += 1
+        self.x_speed += 1
         self.rect.y += self.y_speed
+        self.rect.x += self.x_speed
         if self.rect.bottom > self.scene.bottom:
-            # print('Bye, player!')
-            # self.kill()
             self.y_speed *= -1
             self.rect.y += self.y_speed
-        if self.y_speed < 0:
+        elif self.rect.right > self.scene.right:
+            self.x_speed *= -1
+            self.rect.x += self.x_speed
+        elif self.rect.left < self.scene.left:
+            self.x_speed *= 1
+            self.rect.x -= self.x_speed
+        elif self.y_speed < 0:
             self.y_speed += 0.2
-        # print(f'Player y = {self.rect.y}')
-
     def boost(self):
-        self.y_speed -= 10
+        self.y_speed -= 1
+        #self.x_speed -= 8
