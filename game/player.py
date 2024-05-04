6470fe7d4d1700e3dde5046540520bf55a9d4742
roman.voronov <roman.voronov@axenix.pro>
2024-05-04 14:51:01 +0400

    refactor: universal path for player logo

--- game/player.py
+++ game/player.py
@@ -1,4 +1,10 @@
+from pathlib import Path
+
 import pygame
+
+
+#print(f'{__file__ = }, {Path().absolute() = }')
+CURRENT_FOLDER = Path(__file__).parent
 
 
 class Player(pygame.sprite.Sprite):
@@ -6,13 +12,14 @@ class Player(pygame.sprite.Sprite):
         pygame.sprite.Sprite.__init__(self)
         # self.image = pygame.surface.Surface((30, 30))
         # self.image.fill("red")
-        self.image = pygame.image.load('logo.png')  # .convert()
+        self.image = pygame.image.load(CURRENT_FOLDER / 'logo.png')  # .convert()
         self.rect = self.image.get_rect()
         self.y_speed = 0
 
         self.scene = pygame.display.get_surface().get_rect()
         print(f'area bottom = {self.scene.bottom}')
-        self.rect.x = self.scene.centerx
+        # self.rect.x = self.scene.centerx
+        self.rect.centerx = self.scene.centerx
         self.rect.y = 100
 
     def update(self):
@@ -23,7 +30,9 @@ class Player(pygame.sprite.Sprite):
             # self.kill()
             self.y_speed *= -1
             self.rect.y += self.y_speed
-        print(f'Player y = {self.rect.y}')
+        if self.y_speed < 0:
+            self.y_speed += 0.2
+        # print(f'Player y = {self.rect.y}')
 
     def boost(self):
         self.y_speed -= 10
